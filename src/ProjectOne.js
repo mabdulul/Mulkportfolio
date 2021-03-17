5d6d7875097ed71402d8e733b4ab7c6be97fcd76
Mulk <mabdulul@gmail.com>
2021-03-17 18:25:19 -0400

    ok

--- src/ProjectOne.js
+++ src/ProjectOne.js
@@ -74,7 +74,7 @@ const ProjectOne = () => {
 									{" "}
 									<a
 										className='aLink'
-										href='https://h69sqhfovcwephwc-52517863605.shopifypreview.com'
+										href='https://fm3i9o181pfhophf-52517863605.shopifypreview.com'
 										target='_blank'
 										rel='noopener noreferrer'
 									>
